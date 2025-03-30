bcfe94256418e0fd2d47168bc4655872d9224802
shinta0806 <shinta.0806@gmail.com>
2025-03-30 14:39:29 +0900

    プロジェクト ファイルを追加します。

---
+++ TestTitleBar/Activation/IActivationHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace TestTitleBar.Activation;
+
+public interface IActivationHandler
+{
+    bool CanHandle(object args);
+
+    Task HandleAsync(object args);
+}
---
+++ TestTitleBar/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+using TestTitleBar.ViewModels;
+
+namespace TestTitleBar.Views;
+
+public sealed partial class MainPage : Page
+{
+    public MainViewModel ViewModel
+    {
+        get;
+    }
+
+    public MainPage()
+    {
+        ViewModel = App.GetService<MainViewModel>();
+        InitializeComponent();
+    }
+}
